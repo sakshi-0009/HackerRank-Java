cfaa9bc2fbc87c2c7f17fdb8b005603c02061fd3
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2022-09-27 22:25:15 +0530

    Create Welcome to Java.java

---
+++ Welcome to Java.java
@@ -0,0 +1,12 @@
+# Question : 1.Print Hello, World. on the first line.
+             2.Print Hello, Java. on the second line.
+  
+  # Answer : 
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
