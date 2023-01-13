ca208e68f95cd7aea0b3d5eefd0f8764e3d3d292
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-01-13 19:21:03 +0530

    Create Java Anagrams.java

---
+++ Java Anagrams.java
@@ -0,0 +1,59 @@
+/* Function Description :-
+
+Complete the isAnagram function in the editor.
+
+isAnagram has the following parameters:
+
+string a: the first string
+string b: the second string
+Returns
+
+boolean: If a and b are case-insensitive anagrams, return true. Otherwise, return false.
+Input Format
+
+The first line contains a string a.
+The second line contains a string b.*/
+
+// Answer :-
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String s1, String s2) {
+        // Complete the function
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        
+        if(s1.length()==s2.length())
+
+        {
+            int[] a = new int[256];
+            int[] b = new int[256];
+            for (int i = 0; i < s1.length(); i++) {
+                a[(int) s1.charAt(i)] += 1;
+                b[(int) s2.charAt(i)] += 1;
+            }
+            for (int i = 0; i < 256; i++) {
+                if (a[i] != b[i])
+                    return false;
+
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
