cf6d1cd4d5df178531aff8fd24b0a57dc24ef1e4
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2023-02-27 10:39:43 +0530

    Update java int to string.java

--- java int to string.java
+++ java int to string.java
@@ -1,10 +1,10 @@
-# Question : You are given an integer n, you have to convert it into a string.
+/* Question : You are given an integer n, you have to convert it into a string.
 
 Please complete the partially completed code in the editor. If your code successfully converts n into a string s the code will print "Good job". Otherwise, it will print "Wrong answer".
 
-n can range between -100 to 100 inclusive.
+n can range between -100 to 100 inclusive.*/
 
-# Answer :
+// Answer :
 
 import java.util.*;
 import java.security.*;
